d46d1784be5024b780f700f06c24e40cb579bd95
Jose Mariano <crisant231@gmail.com>
2024-08-30 01:43:01 -0600

    se actualizaron algunos de los doc

---
+++ 5.path.js
@@ -0,0 +1,7 @@
+const path = require('node:path')
+// unir rutas en path
+
+console.log(path.sep)
+// unir rutas
+//const filePath= path.join('content', 'subfolder', 'test.txt')
+//console.log(filePath)
